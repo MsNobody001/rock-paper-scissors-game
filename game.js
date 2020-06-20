388c87fce560987a866bf1998e247f0702632b81
Sara Fojt <sarcia.fojt@gmail.com>
2020-06-21 00:41:36 +0200

    Add game panel and who wins

--- game.js
+++ game.js
@@ -17,7 +17,6 @@ const hands = [...document.querySelectorAll('.select img')];
 // Wybranie ręki przez gracza
 const selectHand = (e) => {
     game.playerHand = e.target.dataset.option;
-    console.log(game.playerHand);
     hands.forEach(hand => hand.style.boxShadow = '');
     e.target.style.boxShadow = '0 0 3px 6px red';
 }
@@ -38,6 +37,27 @@ function checkResult(player, ai) {
     }
 }
 
+function gameResults(player, ai, result) {
+    document.querySelector('[data-summary="your-choice"]').textContent = player;
+    document.querySelector('[data-summary="ai-choice"]').textContent = ai;
+
+    document.querySelector('p.numbers span').textContent = ++gameSummary.numbers;
+
+    if (result === "draw") {
+        document.querySelector('p.draws span').textContent = ++gameSummary.draws;
+        document.querySelector('[data-summary="who-win"]').textContent = 'Remis :/';
+        document.querySelector('[data-summary="who-win"]').style.color = 'gray';
+    } else if (result === "win") {
+        document.querySelector('p.wins span').textContent = ++gameSummary.wins;
+        document.querySelector('[data-summary="who-win"]').textContent = 'Ty wygrałeś!';
+        document.querySelector('[data-summary="who-win"]').style.color = 'green';
+    } else {
+        document.querySelector('p.losses span').textContent = ++gameSummary.loses;
+        document.querySelector('[data-summary="who-win"]').textContent = 'Przegrałeś :(';
+        document.querySelector('[data-summary="who-win"]').style.color = 'red';
+    }
+}
+
 // Funkcja sterująca
 function startGame() {
     if (!game.playerHand) {
@@ -45,6 +65,7 @@ function startGame() {
     }
     game.aiHand = aiChoice();
     const gameResult = checkResult(game.playerHand, game.aiHand);
+    gameResults(game.playerHand, game.aiHand, gameResult);
 }
 
 hands.forEach(hand => hand.addEventListener('click', selectHand));
